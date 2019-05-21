80c5ba311f6a83a23bf735197fbf01a09ea8a46d
Rodrigo <rodrigopantoja17@yahoo.com>
2019-05-21 19:54:40 -0300

    Projeto 01 concluido(add Classes e heranças)

---
+++ Novice.swift
@@ -0,0 +1,13 @@
+class Novice: Mage {
+    init(){
+        self.nick
+        self.hp
+        self.mp
+        self.pet
+    }
+
+    func getPet() -> String {
+        let nPet = readline()
+        return self.pet = nPet
+    }
+}
---
+++ Soldier.swift
@@ -0,0 +1,13 @@
+class Soldier: Warrior {
+    init() {
+        self.nick
+        self.hp
+        self.mp
+        self.armor
+    }
+
+    func getArmor() -> String {
+        let nArmor = readline()
+        return self.armor = nArmor
+    }
+}
